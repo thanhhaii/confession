07048ee0f2cae860e0ff3b1d592b4460b5ad393e
thanhhaii <hailamnguyenthanh@gmail.com>
2023-11-22 00:42:16 +0700

    feat:develop api login

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 	"juliet/initializers"
 	usertransport "juliet/internal/module/user/transport"
+	"juliet/pkg/tokenFactory"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
 	initializers.SyncDatabase()
+}
+
+type Service struct {
+	tokenFactory tokenFactory.TokenFactory
 }
 
 func main() {
@@ -20,11 +26,19 @@ func main() {
 		})
 	})
 
+	service := Service{
+		tokenFactory: tokenFactory.CreateTokenFactory(
+			[]byte("secret-key"),
+			jwt.SigningMethodHS256,
+		),
+	}
+
 	v1 := e.Group("/v1")
 	user := v1.Group("/user")
 	{
 		user.POST("/sign-up", usertransport.HandleSignUp(initializers.DB))
 		user.GET("", usertransport.HandleFindUserByEmail(initializers.DB))
+		user.POST("/sign-in", usertransport.HandleSignIn(initializers.DB, service.tokenFactory))
 	}
 
 	e.Logger.Fatal(e.Start(":1411"))
