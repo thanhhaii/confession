fc729cd092e570cc91f642f58723fb9b1d18813b
thanhhaii <hailamnguyenthanh@gmail.com>
2023-11-27 12:52:12 +0700

    feat: apply echo middleware jwt

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"juliet/initializers"
 	usertransport "juliet/internal/module/user/transport"
 	"juliet/pkg/tokenFactory"
+	"strings"
 )
 
 func init() {
@@ -20,11 +22,24 @@ type Service struct {
 
 func main() {
 	e := echo.New()
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(200, map[string]interface{}{
-			"message": "pong",
+
+	server := e.Group("/server")
+	{
+		server.GET("/ping", func(c echo.Context) error {
+			return c.JSON(200, map[string]interface{}{
+				"message": "pong",
+			})
 		})
-	})
+	}
+
+	e.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte("secret-key"),
+		SigningMethod: echojwt.AlgorithmHS256,
+		Skipper: func(c echo.Context) bool {
+			path := c.Request().URL.Path
+			return strings.Contains(path, "/sign-in") || strings.Contains(path, "/sign-up")
+		},
+	}))
 
 	service := Service{
 		tokenFactory: tokenFactory.CreateTokenFactory(
